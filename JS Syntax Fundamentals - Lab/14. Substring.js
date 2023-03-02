dab99902c588cfb36a8142f4c1c6037298845aa0
Stanislav Bozhimirov <bozhimirov@gmail.com>
2023-03-02 08:22:55 +0200

    solved Sub-string problem

---
+++ JS Syntax Fundamentals - Lab/14. Substring.js
@@ -0,0 +1,8 @@
+function solve(string, startIndex, count) {
+    let result;
+        result = string.slice(startIndex, startIndex + count)
+    console.log(result);
+}
+
+solve('ASentence', 1, 8)
+solve('SkipWord', 4, 7)
