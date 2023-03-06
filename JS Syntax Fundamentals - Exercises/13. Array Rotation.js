0c4f3b77b0d05be4699f5d233c1d113133501658
Stanislav Bozhimirov <bozhimirov@gmail.com>
2023-03-06 23:08:46 +0200

    solved Array Rotation problem

---
+++ JS Syntax Fundamentals - Exercises/13. Array Rotation.js
@@ -0,0 +1,21 @@
+function arrayRotation(numbers, rotations){
+    rotations %= numbers.length;
+
+    for (let i = 0; i < rotations; i++){
+        let first = numbers.shift();
+        numbers.push(first);
+    }
+
+    console.log(numbers.join(' '));
+}
+
+arrayRotation(
+    [51, 47, 32, 61, 21], 2
+);
+arrayRotation(
+    [32, 21, 61, 1], 4
+);
+arrayRotation(
+    [2, 4, 15, 31], 5
+);
+
