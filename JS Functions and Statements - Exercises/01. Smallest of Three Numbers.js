b5803c1fc93a935559da1a1c2c315bfd5f6e65f6
Stanislav Bozhimirov <bozhimirov@gmail.com>
2023-03-11 08:28:21 +0200

    solved Smallest of three numbers problem

---
+++ JS Functions and Statements - Exercises/01. Smallest of Three Numbers.js
@@ -0,0 +1,6 @@
+// function smallestNumber(first, second, third) {
+//     let min = Math.min(first, second, third);
+//     return min
+// }
+
+const smallestNumber = (first, second, third) => Math.min(first, second, third)
