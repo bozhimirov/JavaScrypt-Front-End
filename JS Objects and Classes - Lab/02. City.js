121181a73f239901f985c5748cd1201b285c3957
Stanislav Bozhimirov <bozhimirov@gmail.com>
2023-03-14 17:24:58 +0200

    solved City problem

---
+++ JS Objects and Classes - Lab/02. City.js
@@ -0,0 +1,18 @@
+function cityInfo(city) {
+    let keys = Object.keys(city);
+    for (const key of keys) {
+        console.log(`${key} -> ${city[key]}`)
+    }
+
+}
+
+cityInfo(
+    {
+        name: "Sofia",
+        area: 492,
+        population: 1238438,
+        country: "Bulgaria",
+        postcode: "1000"
+
+    }
+)
