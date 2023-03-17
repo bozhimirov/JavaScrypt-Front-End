58c7f266a22086c277b38408434e02cc50b27c20
Stanislav Bozhimirov <bozhimirov@gmail.com>
2023-03-17 14:14:51 +0200

    solved Cats problem

---
+++ JS Objects and Classes - Lab/09. Cats.js
@@ -0,0 +1,27 @@
+function catSolve(input) {
+    class Cat {
+        constructor(name, age) {
+            this.name = name;
+            this. age = age;
+        }
+
+        meow() {
+            console.log(`${this.name}, age ${this.age} says Meow`)
+        }
+    }
+
+    let cats = [];
+    for (const line of input) {
+        let [ name, age ] = line.split(' ');
+        age = Number(age);
+        cats.push(new Cat(name, age));
+    }
+
+    for (const cat of cats) {
+        cat.meow();
+    }
+}
+
+catSolve(
+    ['Mellow 2', 'Tom 5']
+)
