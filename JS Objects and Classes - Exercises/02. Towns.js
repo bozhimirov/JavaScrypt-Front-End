b0f8aff90f6aeef6d3b33ffb5eb819d911262202
Stanislav Bozhimirov <bozhimirov@gmail.com>
2023-03-18 19:20:18 +0200

    solved Towns problem

---
+++ JS Objects and Classes - Exercises/02. Towns.js
@@ -0,0 +1,28 @@
+function townsParser(input) {
+  input
+    .map((line) => line.split(" | "))
+    .map(([town, latitude, longitude]) => ({
+      town,
+      latitude: Number(latitude).toFixed(2),
+      longitude: Number(longitude).toFixed(2),
+    }))
+    .forEach((townObj) => console.log(townObj));
+}
+
+// function townsParser(input) {
+//   for (const line of input) {
+//     let [town, latitude, longitude] = line.split(" | ");
+//     let townObj = {
+//       town,
+//       latitude: Number(latitude).toFixed(2),
+//       longitude: Number(longitude).toFixed(2),
+//     };
+
+//     console.log(townObj);
+//   }
+// }
+
+townsParser([
+  "Sofia | 42.696552 | 23.32601",
+  "Beijing | 39.913818 | 116.363625",
+]);
