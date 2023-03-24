57a5996c40c3216b85e5b9e542dfc362b1e533cd
Stanislav Bozhimirov <bozhimirov@gmail.com>
2023-03-24 21:23:32 +0200

    solved Subtraction problem

---
+++ JS DOM and Events - Exercises/01. Subtraction/subtract.js
@@ -0,0 +1,6 @@
+function subtract() {
+  const firstNum = Number(document.getElementById("firstNumber").value);
+  const secondNum = Number(document.getElementById("secondNumber").value);
+  const result = document.getElementById("result");
+  result.textContent = firstNum - secondNum;
+}
